d8656b856b00203a523adc78644ba16db20e9be8
Ubuntu <azureuser@FlyMeServer.xfi352pn44feroccjilicelsse.bx.internal.cloudapp.net>
2021-11-02 13:08:30 +0000

    achieve receiving data from IBM Watson

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import Flask
 from flask import request
-
+import json
 app = Flask(__name__)
 
-@app.route("/webhook")
+@app.route("/webhook", methods=['POST', 'GET'])
 def webhook():
-    return request.args
+    params = json.loads(request.data.decode('utf-8'))
+    print(params)
+    return params 
+
 
 @app.route("/")
 def home():
